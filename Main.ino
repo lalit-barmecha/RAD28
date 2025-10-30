e9430218086a9ecf7d805536bb279f70bf4e0c3a
Safal Ghimire <safal_g@outlook.com>
2025-10-30 18:39:55 -0400

    Uploaded the final code used for the Smart Blinds
    This code was used for the presentation of the smart blinds project. The blinds would go up/down based on sensor input such as light and sound.

--- Main.ino
+++ Main.ino
@@ -1,101 +1,116 @@
 #include <Wire.h>
-#include <time.h>
-#include <DS3231.h> //RTC
-#include <U8g2lib.h> //LCD Display
+#include <Adafruit_MotorShield.h> // Motor shield library
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-DS3231 myRTC;
-bool h12;
-bool hPM;
-bool CenturyBit;
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 
-#define A0_Light A0
-#define A1_Audio A1
+const int brightness_lightSensorPin = A0;
+const int open_detectLightSensorPin = A1;
+const int close_detectLightSensorPin = A2;
+const int audio_detect = A3;
 
-#define A2_Light A2
-#define A3_Light A3
 
-#define D1 1
+const int light_threshold = 100; // Adjust after testing
+const int noise_threshold = 700;  // Threshold for noise, adjust as needed.
+const int close_detection_val = 300; // Sensor value when blinds fully closed
+const int open_detection_val = 400;  // Sensor value when blinds fully opened
 
-bool status = false;
-bool preferredStatus = false;
+bool blind_status = false;  // false = blinds open, true = blinds closed
 
-U8G2_SSD1306_64X32_1F_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+// Select motor port M1 (can also be M2, M3, or M4)
+Adafruit_DCMotor *motor = AFMS.getMotor(1); //pointer to motor object (M1 port)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
-  Wire.begin();  
-  Serial.begin(9600); 
-  u8g2.begin();
+   Serial.begin(9600);
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {  // 0x3C is default I2C address
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;); // Loop forever if display doesn't initialize (that way user knows somethings up)
+  }
 
-  //Get the current status of the blinds and display on the LCD display
+  display.clearDisplay();             // Clear the buffer
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Set text color
+  display.setCursor(0, 0);            // Start at top-left corner
+  display.println("Hello, OLED!");
+  display.display();                  // Show the message
+  delay(2000);
 
-  // Setup RTC
-  // bool mode12 = false;
-  // myRTC.setClockMode(mode12);
-  //myRTC.setHour(8);
-  //myRTC.setMinute(44);
-  //myRTC.setSecond(30);
-  u8g2.clearBuffer();					// clear the internal memory
-  u8g2.setFont(u8g2_font_t0_16_tf);
+  AFMS.begin();
+  motor->setSpeed(25); //set speed 
+  motor->run(RELEASE); //motor is stopped
 }
 
 void loop() {
-  //Get current status
-  //Getting all inputs
-  int light = analogRead(A0_Light);
-  int light2 = analogRead(A2_Light);
-  int light3 = analogRead(A3_Light);
   
-  byte theHour = myRTC.getHour(h12, hPM);
-  byte theMinute = myRTC.getMinute();
-  byte theSecond = myRTC.getSecond();
-
-  int audio = analogRead(A1_Audio);
-
-  // Compare sensor values
-  if (light < 300) {
-    preferredStatus = true;
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print("Blinds State: ");
+  if(blind_status)
+  {
+    display.println("Closed");
   }
-  else if (audio > 300) {
-    preferredStatus = true;
+  else 
+  {
+    display.println("Open");
   }
-  //else if (theHour > 16 || theHour < 7) {
-    //preferredStatus = true;
-  //} 
-  else {
-    preferredStatus = false;
-  }
+  display.display();
 
-  if (status != preferredStatus) {
-    u8g2.clearBuffer();					
-    u8g2.setFont(u8g2_font_t0_16_tf); 
-    if (status == false) {
-      u8g2.drawStr(0, 10, "EG1004"); 
-      u8g2.sendBuffer(); 
-      // Send signal to motor
-      status = true;
-    } else {
-      u8g2.drawStr(0, 10, "EG1004");
-      u8g2.sendBuffer(); 
-      // Send signal to motor
-      status = false;
-      delay(10000);
+  delay(1000);
+
+  int brightness = analogRead(brightness_lightSensorPin);
+  int close_check_brightness = analogRead(close_detectLightSensorPin);
+  int open_check_brightness = analogRead(open_detectLightSensorPin);
+  int noise_level = analogRead(audio_detect);
+  Serial.print("Brightness:");
+  Serial.print(brightness);
+  Serial.print(" | Close Sensor: ");
+  Serial.print(close_check_brightness);
+  Serial.print(" | Open Sensor: ");
+  Serial.println(open_check_brightness);
+  Serial.println("| Noise: ");
+  Serial.println(noise_level);
+
+  bool testing = false;
+  delay(1000);
+
+if (!blind_status && (brightness < light_threshold || noise_level > noise_threshold)) {
+  motor->run(BACKWARD);
+  while (true) {
+    close_check_brightness = analogRead(close_detectLightSensorPin);
+    Serial.print("Close Sensor: ");
+    Serial.println(close_check_brightness);
+
+    if (close_check_brightness > close_detection_val) {
+      Serial.println("STOPPING - FULLY CLOSED");
+      motor->run(RELEASE);
+      blind_status = true;
+      break;
+    }
+    delay(5000);
+  }
+}
+else if (blind_status && (brightness >= light_threshold && noise_level <= noise_threshold)) { //if noiselevel is greater than threshold, close. 
+
+  motor->run(FORWARD);
+  while (true) {
+    open_check_brightness = analogRead(open_detectLightSensorPin);
+    Serial.print("Open Sensor: ");
+    Serial.println(open_check_brightness);
+
+    if (open_check_brightness < open_detection_val) {
+      Serial.println("STOPPING - FULLY OPEN");
+      motor->run(RELEASE);
+      blind_status = false;
+      break;
+    }
+      delay(1000);
     }
   }
-  
-  
-  // Tests
-  // Serial.print("light ");
-  // Serial.println(light);
-  // Serial.print("light2 ");
-  // Serial.println(light2);
-  // Serial.print("light3 ");
-  // Serial.println(light3);
-  // Serial.print("Hour");
-  // Serial.println(theHour);
-  // Serial.println(theMinute);
-  // Serial.println(theSecond);
-  // Serial.print("audio");
-  // Serial.println(audio);
-  // Serial.println("");
-  delay(1000);
 }
+
+
+
